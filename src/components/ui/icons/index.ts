8cda06b39ebfca39766ddd77519c2b557c219a9d
starcradle101 <dreamerdev12@gmail.com>
2025-01-29 20:13:27 +0900

    feat(icons): 아이콘 컴포넌트 구현 * GitHub, 메일, 다크모드 토글에 대한 아이콘 컴포넌트화 관리 Related to: #1

---
+++ src/components/ui/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as SunIcon } from './SunIcon';
+export { default as MoonIcon } from './MoonIcon';
+export { default as GitHubIcon } from './GitHubIcon';
+export { default as MailIcon } from './MailIcon';
